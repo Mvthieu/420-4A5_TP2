e81fa8d22f12dc21da753f4eb5d9dd62dd5c1dc7
Mathieu Girard <mgirard514@gmail.com>
2023-04-11 13:41:35 -0400

    Fait: création d'une fonctione qui permet de créer un cours

--- controllers/cours-controleurs.js
+++ controllers/cours-controleurs.js
@@ -20,5 +20,44 @@ const getCoursById = async (req, res, next) => {
     if(!cours){
         return next(new HttpErreur("Aucun cours trouvé pour l'id fourni", 404));
     };
-    rep.json({cours: cours.toObject({getters: true})});
+    res.json({cours: cours.toObject({getters: true})});
 };
+
+const creerCours = async (req, res, next) => {
+    const { titre, discipline, professeur } = req.body;
+    const nouveauCours = new Cours({
+      titre,
+      discipline,
+      nombreMaxEtudiants: 30,
+      dateDebut: "2023-01-01",
+      dateFin: "2023-06-06",
+      etudiants:[],
+      professeur,
+    });
+  
+    let unProfesseur;
+  
+    try {
+        unProfesseur = await Professeur.findById(professeur);
+      
+    } catch {
+      
+      return next(new HttpErreur("Création de cours échouée", 500));
+    }
+  
+    if (!unProfesseur) {
+      return next(new HttpErreur("Professeur non trouvé selon le id"), 504);
+    }
+  
+    try {
+  
+      
+      await nouveauCours.save();
+      unProfesseur.cours.push(nouveauCours);
+      await unProfesseur.save();
+    } catch (err) {
+      const erreur = new HttpErreur("Création de cours échouée", 500);
+      return next(erreur);
+    }
+    res.status(201).json({ cours: nouveauCours });
+  };
